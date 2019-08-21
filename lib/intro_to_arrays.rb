214db33b831ede4c6c664809cf17b3056269a813
Ilita Kunelyte <kunelyte.ilita90@gmail.com>
2019-08-21 10:17:08 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,24 @@
+def instantiate_new_array
+  new = []
+end
+ def array_with_two_elements
+   new = [1,2]
+ end
+ def first_element(new)
+   new.first
+ end
+ def third_element (new)
+   new[2]
+ end
+ def last_element(new)
+   new[-1]
+ end
+ def first_element_with_array_methods(new)
+   new.first
+ end
+ def last_element_with_array_methods(new)
+ new.last
+ end
+ def length_of_array(new)
+   new.length
+ end
