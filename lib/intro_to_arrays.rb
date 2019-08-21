2c7b3812d316f4b27fed6b154fb8820beab702bf
Ilita Kunelyte <kunelyte.ilita90@gmail.com>
2019-08-21 10:14:09 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,3 +1,16 @@
 def instantiate_new_array
   new = []
 end
+ def array_with_two_elements
+   new = [1,2]
+ end
+ def first_element(new)
+   new.first
+ end
+ def third_element (new)
+   new[2]
+ end
+ def last_element(new)
+   new[-1]
+ end
+ 
